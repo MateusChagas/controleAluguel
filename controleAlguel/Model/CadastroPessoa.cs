3f74911aad5dabb6f47f3ddefa509fa4b8066a1a
MateusOliveira2510 <artit.mateus.chagas@lmmobilidade.com.br>
2023-08-20 13:04:23 -0300

    Primeiras Classes do Projeto

---
+++ controleAlguel/Model/CadastroPessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace controleAlguel.Model
+{
+    public class CadastroPessoa
+    {
+        public int Pessoa { get; set; }
+        public string nomeCompleto { get; set; }
+        public string? profissao { get; set; }
+        public int moradores { get; set; }
+        public string renda { get; set; }
+        IList<controleImoveis>? controleImoveis { get; set; }
+
+    }
+}
